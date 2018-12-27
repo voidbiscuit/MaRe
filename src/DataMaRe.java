588fb513dfd710cee2b1daf892adc3fc5fd73f1d
Tim Nicholls <cx004763@live.reading.ac.uk>
2018-12-27 00:28:06 +0000

    Sexy file loading and displaying

--- src/DataMaRe.java
+++ src/DataMaRe.java
@@ -1,51 +1,76 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataMaRe {
+
     // Constants
     private static final String regex_split = ",";
+
     // Individual Data
-    private String file_path;
-    private ArrayList<String> lines;
+    private String
+            file_path,
+            file_name;
+    private ArrayList<ArrayList<String>> data;
     private String[] types;
 
-    public DataMaRe(String file_path) {
+    /**
+     * Object to represent a CSV file, loaded in.
+     *
+     * @param file_path Path of the CSV File
+     */
+    public DataMaRe(String file_path, String file_name) {
         // Update the filepath
-        this.file_path = file_path;
-        // Read the file in by lines
+        setFilePath(file_path, file_name);
+        // Read new data
         readData();
-
-
     }
 
-    private String[] readData() {
+    private void setFilePath(String file_path, String file_name) {
+        this.file_path = file_path;
+        this.file_name = file_name;
+    }
+
+
+    private void readData() {
         // Clear Data
-        ArrayList<String> lines = new ArrayList<>();
+        data = new ArrayList<>();
         try {
-            // Prepare to Read file
-            BufferedReader r = new BufferedReader(new FileReader(new File(file_path)));
-            String line;
-            // Read file to end
-            while ((line = r.readLine()) != null)
-                lines.add(line);
-            return Arrayslines;
+            // Initialise a reader to open the file
+            BufferedReader r = new BufferedReader(new FileReader(new File(this.file_path + this.file_name)));
+            // Load in each line
+            String buffer;
+            while ((buffer = r.readLine()) != null) {
+                buffer = buffer.replace("\\s+", " "); // remove trash whitespace
+                data.add(new ArrayList<>(Arrays.asList(buffer.split(regex_split)))); // Load as arraylists
+            }
         } catch (IOException e) {
             // Catch if fucked
             e.printStackTrace();
         }
     }
 
-    private void putIntoColumns(String... types) {
-        this.types = types;
-        String[] line_fragments;
-        for (String line : lines) {
-            line_fragments = line.split(regex_split);
-            for (int fragment = 0; fragment < line_fragments.length; fragment++) {
-                switch (types[fragment]) {
-                    case :
-                    stuff.add(Strin types[fragment]line_fragments[fragment].)
-                }
-            }
+    public void displayData() {
+        int max = 5;
+        StringBuilder output = new StringBuilder();
+        // Header
+        output.append(String.format("\n\n-- %s\n%d records\n", file_name, data.size()));
+        // For every stored record
+        for (ArrayList<String> record : data) {
+            if (max-- < 0) break; // dirty max
+            // Print a new line
+            output.append("\n\t|");
+            // Print out each fragment, 20 wide
+            for (String fragment : record)
+                output.append(String.format("%20s", fragment));
         }
+        System.out.print(String.format("%150s", output.toString()));
     }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,50 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
-    static final String directory_name = "data/";
-    static ArrayList<String> file_names;
+    // Data
+    private static final String directory_name = "data/";
+
+    // Values
+    private static File directory;
+    private static ArrayList<String> file_names;
+    private static ArrayList<DataMaRe> files;
 
     public static void main(String[] args) {
-        initFiles();
-        new DataMaRe(file_names.get(0));
+        findFiles();
+        if (file_names.size() > 0) {
+            System.out.println(String.format("%d files have been found!", file_names.size()));
+            loadFiles();
+            displayFiles();
+        } else
+            System.out.println("No files found.");
     }
 
 
-    private static boolean initFiles() {
-        File directory = new File(directory_name);
+    private static void findFiles() {
+        directory = new File(directory_name);
         try {
             System.out.println("Loading Files from [" + directory.getCanonicalPath() + "]");
             file_names = new ArrayList<>(Arrays.asList(Objects.requireNonNull(directory.list())));
             if (!file_names.isEmpty())
-                for (int file = 0; file < file_names.size(); file++) {
-                    System.out.println("-- " + file_names.get(file));
-                    file_names.set(file, directory.getCanonicalPath() + "\\" + file_names.get(file));
-                }
-            return true;
+                for (String file_name : file_names) System.out.println("-- " + file_name);
         } catch (
                 IOException e) {
             e.printStackTrace();
         }
-        return false;
+    }
+
+    private static void loadFiles() {
+        files = new ArrayList<>();
+        for (String file_name : file_names)
+            files.add(new DataMaRe(directory_name, file_name));
+    }
+
+    private static void displayFiles() {
+        for (DataMaRe file : files)
+            file.displayData();
     }
 }
